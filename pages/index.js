e517f4d889fa09d69b2cb1a993afaba27ca5bcd1
Mohamad Alabdallah <mohamadalabdallah.uk@gmail.com>
2025-05-11 06:50:33 +0100

    إصلاح الصفحة الرئيسية لتعمل على Vercel

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>مرحبًا بك في ماركت سوريا</h1>
-      <p>موقع الإعلانات المبوبة</p>
+      <p>منصة الإعلانات المبوبة</p>
     </div>
   );
 }
