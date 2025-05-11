4eabf4f74536bba7051c654e4b97bef551ec3542
Mohamad Alabdallah <mohamadalabdallah.uk@gmail.com>
2025-05-11 06:43:56 +0100

    تحديث الصفحة الرئيسية

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// الصفحة الرئيسية - سيتم ملؤها لاحقًا
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>مرحبًا بك في ماركت سوريا</h1>
+      <p>موقع الإعلانات المبوبة</p>
+    </div>
+  );
+}
